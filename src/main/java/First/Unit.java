c1712093e2828233c98543801e66125c928f68c5
DmitriiRybin <Homak9999@gmail.com>
2020-09-24 22:12:41 +0500

    NewTest

---
+++ src/main/java/First/Unit.java
@@ -0,0 +1,12 @@
+package First;
+
+public class Unit {
+    public int Sum (int a, int b){
+        int result = a + b;
+        return result;
+    }
+    public static String TestText() {
+        String text = "HelloWorld";
+        return text;
+    }
+}
